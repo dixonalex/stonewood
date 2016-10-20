6545499916c4a1143c3166ccabc210878a0797db
= <alexanderldixon@gmail.com>
2016-10-19 21:08:16 -0500

    Add vuex and vue-router

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
+Vue.use(VueRouter)
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({

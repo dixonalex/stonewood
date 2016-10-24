dfaf281c19defbe670f2956f6b7a0a1c3eb6fd09
= <alexanderldixon@gmail.com>
2016-10-23 21:24:21 -0500

    WEB-5 #comment add Fieldstone and IStone vendors to our website.

--- src/components/Vendors.vue
+++ src/components/Vendors.vue
@@ -21,6 +21,18 @@
             <span>Wood Harbor</span>
           </a>
         </li>
+        <li>
+          <a href="http://www.fieldstonecabinetry.com/Pages/home.aspx" target='_blank'>
+            <img id="field-stone" class="vendor-logo">
+            <span>Field Stone</span>
+          </a>
+        </li>
+        <li>
+          <a href="http://www.istone.biz/Pages/isthomepg.html" target='_blank'>
+            <img id="istone" class="vendor-logo">
+            <span>iStone</span>
+          </a>
+        </li>
       </ul>
     </div> 
   </section>
@@ -70,16 +82,23 @@ $breakpoint-phone: 480px;
       img {
         display: block;
         margin: 0 auto;
+        width: 100px;
       }
     }
     #mwc {
-      content:url("/static/images/vendors/mwc_b.png");
+      content:url("/static/images/vendors/mwc_c.png");
     }
     #legacy {
-      content:url("/static/images/vendors/legacy_b.png");
+      content:url("/static/images/vendors/legacy_c.png");
     }
     #wh {
       content:url("/static/images/vendors/wh_b.png");
+    }
+    #field-stone {
+      content:url("/static/images/vendors/fieldstone-logo.jpg");
+    }
+    #istone {
+      content:url("/static/images/vendors/istone-logo.gif");
     }
   }
   @media(max-width:$breakpoint-phone){

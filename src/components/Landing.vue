d25382a7c840bfdc9cbcd2e93a930be1ecd99a3a
= <alexanderldixon@gmail.com>
2016-10-25 21:43:44 -0500

    WEB-7 #comment New gallery style added. Need to get feedback from Roy/Sky

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,31 +1,13 @@
 <template>
 <div>
-  <carousel>
-    <slider class="active">
-      <img src="static/images/carousels/landing/1-unknown.jpg">
-      <div class="carousel-caption">
-        <h1>PERFORMANCE EXCELLENCE</h1>
-        <p>HIGH QUALITY CABINETRY & SERVICE SINCE 2006</p>          
-        <router-link to="/about" class="btn btn-primary btn-lg" role="button">SEE MORE</router-link>
-      </div>
-    </slider>
-    <slider>
-      <img src="static/images/carousels/landing/2-unknown.jpg">
-      <div class="carousel-caption">
-        <h1>RESIDENTIAL PRODUCTS</h1>
-          <p>PROFESSIONAL HOME BATH AND KITCHEN DESIGN</p>    
-        <router-link to="/residential" class="btn btn-primary btn-lg" role="button">SEE MORE</router-link>
-      </div>
-    </slider>
-    <slider>
-      <img src="static/images/multi/grand oaks at new smyrna tn.jpg">
-      <div class="carousel-caption">
-        <h1>MULTIFAMILY PROJECTS</h1>
-        <p>PROVEN CABINET AND COUNTER DELIVERY AND INSTALLATION</p>        
-        <router-link to="/multifamily" class="btn btn-primary btn-lg" role="button">SEE MORE</router-link>
-      </div>
-    </slider>
-  </carousel>
+  <transition name="slide-fade-up">
+    <header v-if="showHeader">
+        <h1>StoneWood Sales</h1>
+        <p>Kitchens, Bathrooms, Cabinets, Countertops</p>
+    </header>
+  </transition>
+  <Gallery style="margin-top: 2.5%;" :img-objs="imgObjs"></Gallery>
+  <br>
   <Vendors></Vendors>
   <schedule-appt></schedule-appt>
 </div>
@@ -35,6 +17,7 @@
 import { carousel, slider } from 'vue-strap'
 import Vendors from './Vendors'
 import ScheduleAppt from './Contact/ScheduleAppt'
+import Gallery from './Utils/Gallery'
 export default {
 
   name: 'Landing',
@@ -43,16 +26,74 @@ export default {
     carousel,
     slider,
     Vendors,
-    ScheduleAppt
+    ScheduleAppt,
+    Gallery
+  },
+
+  mounted: function () {
+    this.$nextTick(function () {
+      // code that assumes this.$el is in-document
+      setTimeout(function () { this.showHeader = false }.bind(this), 2000)
+    })
   },
 
   data () {
     return {
-
+      showHeader: true,
+      imgObjs: [{
+        Src: 'static/images/carousels/landing/1-unknown.jpg',
+        Title: 'PERFORMANCE EXCELLENCE',
+        Description: 'HIGH QUALITY CABINETRY & SERVICE SINCE 2006',
+        Display: true
+      },
+      {
+        Src: 'static/images/carousels/landing/2-unknown.jpg',
+        Title: 'RESIDENTIAL PRODUCTS',
+        Description: 'PROFESSIONAL HOME BATH AND KITCHEN DESIGN',
+        Display: false
+      },
+      {
+        Src: 'static/images/multi/grand oaks at new smyrna tn.jpg',
+        Title: 'MULTIFAMILY PROJECTS',
+        Description: 'PROVEN CABINET AND COUNTER DELIVERY AND INSTALLATION',
+        Display: false
+      }]
     }
   }
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="sass" scoped>
+// HEADER
+header
+  background-color: red
+  color: #fff
+  text-align: center
+  padding: 30px 0 90px
+
+  h1
+    text-align: center
+    text-transform: uppercase
+    font-size: 65px
+    font-weight: 400
+    letter-spacing: 3px
+    line-height: .8
+    padding-top: 50px
+    font-family: 'Montserrat', sans-serif
+
+.slide-fade-up-enter-active
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+.slide-fade-up-leave-active
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+.slide-fade-up-enter, .slide-fade-up-leave-active
+  padding-top: 10px
+  opacity: 0
+.fade-enter-active, .fade-leave-active
+  transition: opacity .5s
+.fade-enter, .fade-leave-active
+  opacity: 0
+
+h2
+  color: #0a0a0a;
+  text-align: center;
 </style>

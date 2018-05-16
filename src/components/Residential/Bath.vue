9f6dfb1bfd334147c1facea330f0bffd4a4d6613
Alex Dixon <alexanderldixon@gmail.com>
2018-05-15 20:08:55 -0500

    fix broken carousel
    + add facebook scripts to vcs
    + replace vue-carousel with vue-caoursel-3d

--- src/components/Residential/Bath.vue
+++ src/components/Residential/Bath.vue
@@ -2,35 +2,59 @@
 <div class="Vue-Carousel">
   <!-- <h1>Residential Kitchens</h1> -->
   <div>
-    <carousel :navigationEnabled="true" :perPage="1" :autoplayEnabled="true" :paginationPadding="5" >
-      <slide v-for="pic in Pics">
+    <carousel-3d :width="windowWidth * .7" :height="windowHeight * .7" :controls-visible="true">
+      <slide v-for="(pic, i) in Pics" :index="i">
         <img class="img-responsive" alt="custom bath construction remodel residential" title="Residential Bath" :src=pic.src>
-        <span class="label">
-        </span>
       </slide>
-    </carousel>
+    </carousel-3d>
   </div>
 </div>
-  
+
 </template>
 
 <script>
-import { Carousel, Slide } from 'vue-carousel'
+import { Carousel3d, Slide } from 'vue-carousel-3d'
 const bathSrc = 'static/img/residential/bath/'
 export default {
   components: {
-    Carousel,
+    Carousel3d,
     Slide
   },
-  name: 'Kitchen',
+  name: 'Bath',
   data () {
     return {
+      windowWidth: 0,
+      windowHeight: 0,
       Pics: [
         { src: bathSrc + '1_RMD_MPL_GLR_72M.jpg' },
         { src: bathSrc + '33_BPT_MPL_MID_72M.jpg' },
         { src: bathSrc + '44_SAV_MPL_SNDSLT_72M.jpg' }
       ]
     }
+  },
+  mounted () {
+    this.$nextTick(function () {
+      window.addEventListener('resize', this.getWindowWidth)
+      window.addEventListener('resize', this.getWindowHeight)
+
+      // Init
+      this.getWindowWidth()
+      this.getWindowHeight()
+    })
+  },
+
+  methods: {
+    getWindowWidth (event) {
+      this.windowWidth = document.documentElement.clientWidth
+    },
+
+    getWindowHeight (event) {
+      this.windowHeight = document.documentElement.clientHeight
+    }
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.getWindowWidth)
+    window.removeEventListener('resize', this.getWindowHeight)
   }
 }
 </script>
